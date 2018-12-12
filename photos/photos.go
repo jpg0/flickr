e8d21a8db1ee68f1bab6cccb49f7bdc1835aebd9
Jonathan Gilbert <dev@trillica.com>
2018-12-12 14:38:51 +1100

    added extra operations

--- photos/photos.go
+++ photos/photos.go
@@ -2,19 +2,20 @@ package photos
 
 import (
 	"github.com/jpg0/flickr"
+	"time"
 )
 
 type PhotoInfo struct {
-	Id             string `xml:"id,attr"`
-	Secret         string `xml:"secret,attr"`
-	Server         string `xml:"server,attr"`
-	Farm           string `xml:"farm,attr"`
-	DateUploaded   string `xml:"dateuploaded,attr"`
-	IsFavorite     bool   `xml:"isfavorite,attr"`
-	License        string `xml:"license,attr"`
+	Id           string `xml:"id,attr"`
+	Secret       string `xml:"secret,attr"`
+	Server       string `xml:"server,attr"`
+	Farm         string `xml:"farm,attr"`
+	DateUploaded string `xml:"dateuploaded,attr"`
+	IsFavorite   bool   `xml:"isfavorite,attr"`
+	License      string `xml:"license,attr"`
 	// NOTE: one less than safety level set on upload (ie, here 0 = safe, 1 = moderate, 2 = restricted)
 	//       while on upload, 1 = safe, 2 = moderate, 3 = restricted
-	SafetyLevel    int    `xml:"safety_level,attr"` 
+	SafetyLevel    int    `xml:"safety_level,attr"`
 	Rotation       int    `xml:"rotation,attr"`
 	OriginalSecret string `xml:"originalsecret,attr"`
 	OriginalFormat string `xml:"originalformat,attr"`
@@ -64,6 +65,33 @@ type PhotoInfoResponse struct {
 	Photo PhotoInfo `xml:"photo"`
 }
 
+type PhotoSearchResponse struct {
+	flickr.BasicResponse
+	PhotoList struct {
+		Page    int         `xml:"page,attr"`
+		Pages   int         `xml:"pages,attr"`
+		PerPage int         `xml:"perpage,attr"`
+		Total   int         `xml:"total,attr"`
+		Photos  []PhotoInfo `xml:"photo"`
+	} `xml:"photos"`
+}
+
+type PhotoAllContextsResponse struct {
+	flickr.BasicResponse
+	PhotoAllContexts
+}
+
+type PhotoAllContexts struct {
+	Sets []struct {
+		ID    int    `xml:"id,attr"`
+		Title string `xml:"title,attr"`
+	} `xml:"set" json:"set,omitempty"`
+}
+
+const (
+	mysql_layout = "2006-01-02 15:04:05"
+)
+
 // Delete a photo from Flickr
 // This method requires authentication with 'delete' permission.
 func Delete(client *flickr.FlickrClient, id string) (*flickr.BasicResponse, error) {
@@ -75,6 +103,33 @@ func Delete(client *flickr.FlickrClient, id string) (*flickr.BasicResponse, erro
 	client.OAuthSign()
 
 	response := &flickr.BasicResponse{}
+	err := flickr.DoPost(client, response)
+	return response, err
+}
+
+// Search for photos on Flickr
+func Search(client *flickr.FlickrClient, authenticate bool, user_id string, min_upload_date time.Time, max_upload_date time.Time) (*PhotoSearchResponse, error) {
+	client.Init()
+	client.EndpointUrl = flickr.API_ENDPOINT
+	client.HTTPVerb = "POST"
+	client.Args.Set("method", "flickr.photos.search")
+	client.Args.Set("user_id", user_id)
+	zeroTime := &time.Time{}
+
+	if !min_upload_date.Equal(*zeroTime) {
+		client.Args.Set("min_upload_date", min_upload_date.Format(mysql_layout))
+	}
+	if !max_upload_date.Equal(*zeroTime) {
+		client.Args.Set("max_upload_date", max_upload_date.Format(mysql_layout))
+	}
+	// sign the client for authentication and authorization
+	if authenticate {
+		client.OAuthSign()
+	} else {
+		client.ApiSign()
+	}
+
+	response := &PhotoSearchResponse{}
 	err := flickr.DoPost(client, response)
 	return response, err
 }
@@ -92,6 +147,23 @@ func GetInfo(client *flickr.FlickrClient, id string, secret string) (*PhotoInfoR
 	client.OAuthSign()
 
 	response := &PhotoInfoResponse{}
+	err := flickr.DoPost(client, response)
+	return response, err
+}
+
+// Get information about a Flickr photo
+func GetAllContexts(client *flickr.FlickrClient, id string, secret string) (*PhotoAllContextsResponse, error) {
+	client.Init()
+	client.EndpointUrl = flickr.API_ENDPOINT
+	client.HTTPVerb = "POST"
+	client.Args.Set("method", "flickr.photos.getAllContexts")
+	client.Args.Set("photo_id", id)
+	if secret != "" {
+		client.Args.Set("secret", secret)
+	}
+	client.OAuthSign()
+
+	response := &PhotoAllContextsResponse{}
 	err := flickr.DoPost(client, response)
 	return response, err
 }
